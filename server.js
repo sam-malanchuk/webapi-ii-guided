5f62f8c9125143dbc77af881c3bf3f79c57a32d2
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 14:56:17 -0400

    completed code along with lecture

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const HubsRouter = require('./hubs/hubs-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/hubs', HubsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+module.exports = server;
